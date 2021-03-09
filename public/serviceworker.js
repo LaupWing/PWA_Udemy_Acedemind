2e7fc39560aa11a95276430c520c0527c579690b
LaupWing <loc_strict@hotmail.com>
2021-03-09 20:09:14 +0100

    65

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,11 +1,17 @@
 self.addEventListener('install', function(event){
    console.log('[Service worker] Installing Service Worker', event)
+   event.waitUntil(
+      caches.open('static')
+         .then(function(cache){
+            console.log('[Service worker] Precaching App Shell')
+            cache.add('/src/js/app.js')
+         })
+   )
 })
 self.addEventListener('activate', function(event){
    console.log('[Service worker] Activating Service Worker', event)
    return self.clients.claim()
 })
-
 self.addEventListener('fetch', function(event){
    event.respondWith(fetch(event.request))
 })
