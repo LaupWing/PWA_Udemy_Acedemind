4b66d6a6c92f7dabdeab0e8a17a65f66957f1a8a
LaupWing <loc_strict@hotmail.com>
2021-03-10 15:17:43 +0100

    74

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,7 +1,10 @@
+const CACHE_STATIC_NAME = 'static-v2'
+const CACHE_DYNAMIC_NAME = 'dynamic-v2'
+
 self.addEventListener('install', function(event){
    console.log('[Service worker] Installing Service Worker', event)
    event.waitUntil(
-      caches.open('static')
+      caches.open(CACHE_STATIC_NAME)
          .then(function(cache){
             console.log('[Service worker] Precaching App Shell')
             cache.addAll([
@@ -26,7 +29,7 @@ self.addEventListener('activate', function(event){
       caches.keys()
          .then(function(keyList){
             return Promise.all(keyList.map(function(key){
-               if(key !== 'static-v2' && key !== 'dynamic'){
+               if(key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME){
                   console.log('[Service worker] Removing old cache')
                   return caches.delete(key)
                }
@@ -45,7 +48,7 @@ self.addEventListener('fetch', function(event){
             }else{
                return fetch(event.request)
                   .then(function(res){
-                     return caches.open('dynamic')
+                     return caches.open(CACHE_DYNAMIC_NAME)
                         .then(function(cache){
                            cache.put(event.request.url, res.clone())
                            return res
