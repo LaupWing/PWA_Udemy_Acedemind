12f53190eec13e5114be55a3ec4e40d2500f712e
LaupWing <loc_strict@hotmail.com>
2021-03-11 21:23:04 +0100

    88

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -66,16 +66,43 @@ self.addEventListener('activate', function(event){
 
 
 self.addEventListener('fetch', function(event){
-   event.respondWith(
-      caches.open(CACHE_DYNAMIC_NAME)
-         .then(function(cache){
-            return fetch(event.request)
-               .then(res=>{
-                  cache.put(event.request, res.clone())
-                  return res
-               })
-         })
-   )
+   if(event.request.url === 'https://httpbin.org/get'){
+      event.respondWith(
+         caches.open(CACHE_DYNAMIC_NAME)
+            .then(function(cache){
+               return fetch(event.request)
+                  .then(res=>{
+                     cache.put(event.request, res.clone())
+                     return res
+                  })
+            })
+      )
+   }else{
+      event.respondWith(
+         caches.match(event.request)
+            .then(function(response){
+               if(response){
+                  return response 
+               }else{
+                  return fetch(event.request)
+                     .then(function(res){
+                        return caches.open(CACHE_DYNAMIC_NAME)
+                           .then(function(cache){
+                              cache.put(event.request.url, res.clone())
+                              return res
+                           })
+                     })
+                     .catch(function(err){
+                        return caches.open(CACHE_STATIC_NAME)
+                           .then(function(cache){
+                              return cache.match('/offline.html')
+                           })
+   
+                     })
+               }
+            })
+      )
+   }
 })
 
 // self.addEventListener('fetch', function(event){
